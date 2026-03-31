81bf06db0c30f1da42bfbcdc43ad0d94491a9114
Alex Akere <ojayakere@gmail.com>
2026-03-30 22:57:31 -0600

    Update Node.java

--- src/sait/sll/utility/Node.java
+++ src/sait/sll/utility/Node.java
@@ -2,6 +2,15 @@ package sait.sll.utility;
 
 import java.io.Serializable;
 
+/**
+ * Name: Cam Carter
+ * Date: March 30, 2026
+ * Description: Acts as the fundamental building block for the Singly Linked List. 
+ * Each node encapsulates a data object and a reference to the next node in the chain. 
+ * It is marked as Serializable to ensure the entire linked structure can be 
+ * saved and loaded as a single object.
+ */
+
 public class Node implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
